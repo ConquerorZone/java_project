a039060b4fdc89e671e4474d83a311e5d0f4f144
ConquerorZone <harshkhandekar9988@gmail.com>
2024-11-17 19:52:49 +0530

    renaming file as Anagrams

--- Anagrams.java
+++ Anagrams.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class StringAnagrams {
+public class Anagrams {
 
     static boolean isAnagram(String a, String b) {
         // Complete the function
